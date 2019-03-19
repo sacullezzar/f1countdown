c96835521159e7103eb67c1526d5c36a796fbb1e
tomlightfoot <tomlightfoot96@gmail.com>
2019-03-19 22:45:21 +0000

    maybe use jest to test components

---
+++ tests/first.test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import App from '../containers/App.js'
+
+describe('<App />', function () {
+    it('should match snapshot', function () {
+        const wrapper = shallow(<App />)
+        expect(wrapper).toMatchSnapshot()
+    })
+
+    it('should match snapshot', function () {
+        const wrapper = shallow(<App />)
+        wrapper.instance().handleClick()
+        expect(wrapper).toMatchSnapshot()
+    })
+})
